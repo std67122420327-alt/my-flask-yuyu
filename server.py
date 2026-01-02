0ed286a9dad2db277e6fe6b128c72886f7573af6
std67122420327-alt <std.67122420327@ubru.ac.th>
2026-01-02 20:34:57 +0700

    Update server.py

--- server.py
+++ server.py
@@ -5,22 +5,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html' ,title='My Home Page' ,msg='This is my Message')
 
-
 @app.route('/user/info')
 def info():
-     return render_template('info.html',title='My info',name='suntisouk', email='suntisoukvongsavad@gmail.com',mobile='0988307719',age=21)
+     return render_template('info.html',title='My info',name='manatsanan', email='cream.beem203@gmail.com',mobile='0646919153',age=19)
 
 @app.route('/favorite/sports')
 def fav_sports():
-     sports = ['football', 'basketballl', 'american Football']
+     sports = ['football', 'basketballl', 'ballet']
      return render_template('favorite_sport.html',title='My favorite sports page',sports=sports)
 
 @app.route('/favorite/foods')
 def fav_foods():
-     foods = ['timsom', 'khaopat', 'kaojee','khaidao','padthai']
+     foods = ['kareraitu', 'kushiyaki', 'okonomiyaki','omurice','tempura']
      return render_template('favorite_foods.html',title='My favprite foods page',foods=foods)
 
 @app.route('/favorite/movie')
 def fav_movie():
-     movie = ['ชีนจัง', 'โคนัน', 'โดเดมอน','ป่านางเสือ','คิดตี้']
+     movie = ['inazuma eleven', 'Free!', 'Diabolik lovers','Love Live! School Idol Project','La Storia Della Arcna Famiglia']
      return render_template('favorite_movie.html',title='My favprite movie page',movie=movie)
